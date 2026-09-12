602ff761176255e73b05f453668ae82a1e06b22e
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2026-09-12 11:33:31 -0400

    under construction........

--- panicker-design/app/page.tsx
+++ panicker-design/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <h2>Chris Panicker is a New York-based editorial designer and developer.</h2>
-      <h2>This website is under construction...</h2>
+      <h2>This website is under construction....</h2>
     </div>
   );
 }
